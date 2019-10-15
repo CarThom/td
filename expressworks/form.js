af9272134226137145216e1676ba27a4f5168e36
CarThom <56533745+CarThom@users.noreply.github.com>
2019-10-15 11:01:43 +0200

    Create form.js

---
+++ expressworks/form.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = process.argv[2]
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.post('/form', function(req, res) {
+  res.send(req.body.str.split('').reverse().join(''))
+})
+app.listen(port);
